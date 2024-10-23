a8d02cc4309b4d3fe883ae6eb51c2d6089dde988
Meltem Ceran <meltem.ceran@bilgeadamboost.com>
2024-10-23 17:21:09 +0300

    Add project files.

---
+++ MVCProjectIntro/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCProjectIntro.Models;
+
+namespace MVCProjectIntro.Controllers
+{
+    public class ProductController : Controller
+    {
+        private static List<Product> products = new List<Product>()
+        {
+            new Product {Id= 1 ,Name = "Sandalye", Price=100 },
+            new Product {Id= 2 ,Name = "Yulaf Sütü", Price=200 },
+            new Product {Id= 3 ,Name = "Mouse", Price=150 }
+        };
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View(products);
+        }
+    }
+}
